8d31ed8a0edad5942d70a2969b0af23b6aeceddc
Jasper Mayone <me@jaspermayone.com>
2024-01-15 21:13:38 -0500

    :bug: Fix bugs

--- src/func/checks/alreadyApplied.js
+++ src/func/checks/alreadyApplied.js
@@ -7,13 +7,11 @@ const pizzaAirtable = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(
 
 const alreadyApplied = async (email) => {
   const records = await pizzaAirtable
+  // fetch the record of the user from the airtable base (by email) then console log the IP address of the user
     .select({
-      // filterByFormula: `AND({Email} = '${email}', {Status} = 'Approved')`
-      filterByFormula: `AND({Email} = '${email}')`
+      filterByFormula: `{Email} = '${email}'`
     })
     .all();
-
-console.log(records)
 
 // check to see if records is an empty array
 if (records.length === 0) {
